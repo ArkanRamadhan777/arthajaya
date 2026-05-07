d4799d92f396cc579ca098c148e83a05046bc022
ArkanRamadhan777 <fastforreflek@gmail.com>
2026-05-07 21:48:14 +0700

    fix: remove unused navbar import and clean up dashboard layout

--- src/components/layout/DashboardLayout.tsx
+++ src/components/layout/DashboardLayout.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import { useState } from 'react'
 import Sidebar from './Sidebar'
-import Navbar from './Navbar'
 import { Menu } from 'lucide-react'
 
 export default function DashboardLayout() {
